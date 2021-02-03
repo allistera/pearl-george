450a4a8a65c0ab638c5d84a37b8f1fdbe32d3aee
Allister Antosik <allistera@gmail.com>
2021-02-03 11:11:46 +0000

    Update contact.tsx

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -4,9 +4,7 @@ import styled from "@emotion/styled"
 import Divider from "../elements/divider"
 import Inner from "../elements/inner"
 import Content from "../elements/content"
-import SVG from "./svg"
 import { UpDown, UpDownWide, waveAnimation } from "../styles/animations"
-import Footer from "./footer"
 // @ts-ignore
 import ContactMDX from "../sections/contact"
 
@@ -107,22 +105,8 @@ export default function Contact(offset) {
                 </p>
             </form>
         </Inner>
-        <Footer />
         </Content>
         <Divider speed={0.1} offset={offset} factor={factor}>
-        <UpDown>
-            <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="70%" top="20%" />
-            <SVG icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
-        </UpDown>
-        <UpDownWide>
-            <SVG icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
-            <SVG icon="circle" width={6} color="icon_brightest" left="85%" top="15%" />
-            <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" width={6} color="icon_brightest" left="4%" top="20%" />
-        <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
-        <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
-        <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
         </Divider>
     </div>
 
