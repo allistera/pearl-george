a7fd9e2cf29511e29a275262a045b8163001021e
Allister Antosik <allistera@gmail.com>
2021-02-03 10:26:33 +0000

    Update contact.tsx

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -16,46 +16,115 @@ const InnerWave = styled.div`
   }
 `
 
-const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
-  <div>
-    <Divider fill="divider" speed={0.2} offset={offset} factor={factor}>
-      <div sx={{ position: `absolute`, bottom: 0, width: `full`, transform: `matrix(1, 0, 0, -1, 0, 0)` }}>
-        <InnerWave sx={{ position: `relative`, height: `full`, svg: { width: `100%`, height: `40vh` } }}>
-          <svg xmlns="http://www.w3.org/2000/svg" id="contact-wave" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-            <path>
-              <animate
-                attributeName="d"
-                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                repeatCount="indefinite"
-                dur="30s"
-              />
-            </path>
-          </svg>
-        </InnerWave>
-      </div>
-    </Divider>
-    <Content speed={0.4} offset={offset} factor={factor}>
-      <Inner>
-        <ContactMDX />
-      </Inner>
-      <Footer />
-    </Content>
-    <Divider speed={0.1} offset={offset} factor={factor}>
-      <UpDown>
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="70%" top="20%" />
-        <SVG icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
-      </UpDown>
-      <UpDownWide>
-        <SVG icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
-        <SVG icon="circle" width={6} color="icon_brightest" left="85%" top="15%" />
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
-      </UpDownWide>
-      <SVG icon="circle" width={6} color="icon_brightest" left="4%" top="20%" />
-      <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
-      <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
-      <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
-    </Divider>
-  </div>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-export default Contact
+export default function Contact(offset) {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  return (
+    <div>
+        <Divider fill="divider" speed={0.2} offset={offset} factor={factor}>
+        <div sx={{ position: `absolute`, bottom: 0, width: `full`, transform: `matrix(1, 0, 0, -1, 0, 0)` }}>
+            <InnerWave sx={{ position: `relative`, height: `full`, svg: { width: `100%`, height: `40vh` } }}>
+            <svg xmlns="http://www.w3.org/2000/svg" id="contact-wave" viewBox="0 0 800 338.05" preserveAspectRatio="none">
+                <path>
+                <animate
+                    attributeName="d"
+                    values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
+                    repeatCount="indefinite"
+                    dur="30s"
+                />
+                </path>
+            </svg>
+            </InnerWave>
+        </div>
+        </Divider>
+        <Content speed={0.4} offset={offset} factor={factor}>
+        <Inner>
+            <ContactMDX />
+
+            <form
+                name="contact"
+                method="post"
+                action="/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
+            >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                <label>
+                    Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+                </label>
+                </p>
+                <p>
+                <label>
+                    Your name:
+                    <br />
+                    <input type="text" name="name" onChange={handleChange} />
+                </label>
+                </p>
+                <p>
+                <label>
+                    Your email:
+                    <br />
+                    <input type="email" name="email" onChange={handleChange} />
+                </label>
+                </p>
+                <p>
+                <label>
+                    Message:
+                    <br />
+                    <textarea name="message" onChange={handleChange} />
+                </label>
+                </p>
+                <p>
+                <button type="submit">Send</button>
+                </p>
+            </form>
+        </Inner>
+        <Footer />
+        </Content>
+        <Divider speed={0.1} offset={offset} factor={factor}>
+        <UpDown>
+            <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="70%" top="20%" />
+            <SVG icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
+        </UpDown>
+        <UpDownWide>
+            <SVG icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
+            <SVG icon="circle" width={6} color="icon_brightest" left="85%" top="15%" />
+            <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+        </UpDownWide>
+        <SVG icon="circle" width={6} color="icon_brightest" left="4%" top="20%" />
+        <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
+        <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
+        <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
+        </Divider>
+    </div>
+
+  )
+}
