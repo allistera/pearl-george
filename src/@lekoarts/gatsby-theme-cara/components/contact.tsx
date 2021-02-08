04b83b5d3e2a450ae464fbfee3108d221d3ae5c5
Allister Antosik <allistera@gmail.com>
2021-02-08 11:01:54 +0000

    Update contact.tsx

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
       <form
         name="contact"
         method="post"
-        action="/thanks/"
+        action="/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         onSubmit={handleSubmit}
